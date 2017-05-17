6c4207b81ca32fc50c44dea1dff88d912cf22d22
Shumon Chattopadhyay <s.chattopadhyay89@gmail.com>
2017-05-17 12:35:14 -0400

    Reflects changes made to the Twilio client

--- send-text.py
+++ send-text.py
@@ -1,12 +1,12 @@
-from twilio.rest import TwilioRestClient
+from twilio.rest import Client
 
 # Your Account Sid and Auth Token
 account_sid = ""
 auth_token = ""
-client = TwilioRestClient(account_sid, auth_token)
+client = Client(account_sid, auth_token)
 
-message = client.sms.messages.create(
-    body = "This is a sample text.",
+message = client.messages.create(
+    body = "Hellooooo from the other siiiiiiiide",
     to = "",     # Replace with your phone number
     from_ = "")  # Replace with your Twilio number
-print message.sid
+print(message.sid)
